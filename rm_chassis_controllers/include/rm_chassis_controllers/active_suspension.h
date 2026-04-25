e0c27caf46fdbfe943f42802e5a23ef2bc858c55
ZiDXie <shengxie47@gmail.com>
2026-04-25 09:44:41 +0000

    Temp.

--- rm_chassis_controllers/include/rm_chassis_controllers/active_suspension.h
+++ rm_chassis_controllers/include/rm_chassis_controllers/active_suspension.h
@@ -29,24 +29,72 @@ public:
   void ActiveSuspensionCallBack(const rm_msgs::ChassisActiveSusCmd& msg);
 
 private:
+  double calculateFx_gravity();
+
+  /**
+   * @brief Compute leg polar coordinates (length and angle) from the four-link suspension geometry.
+   *
+   * Given the linkage angles, this helper solves the four-bar configuration and outputs the
+   * equivalent leg length and leg angle used by the controller.
+   *
+   * @param[in]  phi1 Upper/hip link angle (rad).
+   * @param[in]  phi4 Actuated link angle (rad).
+   * @param[out] pos  Output array where:
+   *                 - @c pos[0] is the leg length $L$ (m)
+   *                 - @c pos[1] is the leg angle $\theta$ (rad)
+   *                 The buffer must provide at least 2 elements.
+   *
+   * @note Member link lengths @c l1_..@c l5_ must be configured before calling.
+   */
+  void calculateLegPos(double phi1, double phi4, double pos[2]);
+  double calculateSpringForce();
+
+  /**
+   * @brief Compute joint torque from an equivalent axial/virtual force using $\tau = J^T F$.
+   *
+   * The Jacobian is obtained from calculateJacobian() for the current linkage angles, then the
+   * commanded force is mapped into an actuator torque.
+   *
+   * @param[in]  F    Virtual/axial force applied along the leg (N).
+   * @param[in]  phi1 Hip/upper link angle (rad).
+   * @param[in]  phi4 Actuated link angle (rad).
+   * @param[out] T    Resulting actuator torque (N·m).
+   *
+   * @note The current function signature passes @p T by value; if the intention is to return the
+   *       computed torque to the caller, change it to a reference (e.g. @c double& T).
+   */
+  void calculateTorque(double F, double phi1, double phi4, double T);
+
+  /**
+   * @brief Compute Jacobian terms of the suspension four-link (four-bar) mechanism.
+   *
+   * This helper solves the internal linkage geometry and computes Jacobian elements that are
+   * later used for force/velocity mapping in the virtual-model-control (VMC) formulation.
+   *
+   * @param[in]  phi1 Hip/upper link angle (rad).
+   * @param[in]  phi4 Actuated link angle (rad).
+   * @param[out] J    Output Jacobian storage.
+   *                The current implementation writes to @c J[0][1] and @c J[1][1], therefore the
+   *                provided buffer must be valid for indices at least up to @c [1][1] (i.e. a
+   *                2x2 array or larger backing storage).
+   *
+   * @note Member link lengths @c l1_, @c l2_, @c l3_, @c l4_ must be configured before calling.
+   */
+  void calculateJacobian(double phi1, double phi4, double J[1][2]);
   void moveJoint(const ros::Time& time, const ros::Duration& period) override;
 
-  std::vector<std::shared_ptr<effort_controllers::JointPositionController>> active_suspension_joints_;
-  std::vector<hardware_interface::JointHandle> active_suspension_joint_handles_{};
+  std::shared_ptr<hardware_interface::JointHandle> right_hip_joint_handle_;
+  std::shared_ptr<hardware_interface::JointHandle> left_hip_joint_handle_;
 
   ros::Subscriber active_suspension_sub_;
-  double current_pos_{ 0. };
-  double target_pos_{ 0. };
-  double feedforward_offset{ 0. };
-  double feedforward_effort{ 0. };
-  double stretch_coff_A_{ 0. };
-  double stretch_coff_k_{ 0. };
-  double shrink_coff_A_{ 0. };
-  double shrink_coff_k_{ 0. };
-  double feedforward_effect_time_{ 0. };
-  double static_effort{ 0. };
+  double l1_{ 0. }, l2_{ 0. }, l3_{ 0. }, l4_{ 0. }, l5_{ 0. };
+  double phi4_offset_{ 0. };
+  double left_pos_[2]{ 0. }, right_pos_[2]{ 0. };
+  double target_leg_length_{ 0. };
+  double target_pitch{ 0. };
 
-  ros::Time feedforward_timer;
+  std::shared_ptr<control_toolbox::Pid> pid_roll_{}, pid_pitch_{}, pid_legs_{};
+
   State current_state_{ State::DOWN };
   State last_state_{ State::DOWN };
 };
--- rm_chassis_controllers/src/active_suspension.cpp
+++ rm_chassis_controllers/src/active_suspension.cpp
@@ -9,80 +9,147 @@ namespace rm_chassis_controllers
 bool ActiveSuspensionController::init(hardware_interface::RobotHW* robot_hw, ros::NodeHandle& root_nh,
                                       ros::NodeHandle& controller_nh)
 {
-  OmniController::init(robot_hw, root_nh, controller_nh);
+  if (!OmniController::init(robot_hw, root_nh, controller_nh))
+    return false;
+
+  auto pid_init = [&](const std::string& ns, std::shared_ptr<control_toolbox::Pid>& pid) {
+    if (controller_nh.hasParam(ns))
+    {
+      pid = std::make_shared<control_toolbox::Pid>();
+      if (!pid->init(ros::NodeHandle(controller_nh, ns)))
+        ROS_ERROR_STREAM("Failed to init " << ns);
+    }
+  };
+  pid_init("pid_roll", pid_roll_);
+  pid_init("pid_pitch", pid_pitch_);
+  pid_init("pid_legs", pid_legs_);
+
+  try
+  {
+    left_hip_joint_handle_ =
+        std::make_shared<hardware_interface::JointHandle>(effort_joint_interface_->getHandle("left_hip_joint"));
+    right_hip_joint_handle_ =
+        std::make_shared<hardware_interface::JointHandle>(effort_joint_interface_->getHandle("right_hip_joint"));
+  }
+  catch (const std::exception& e)
+  {
+    ROS_ERROR_STREAM("Failed to get leg joint handle(s): " << e.what());
+    return false;
+  }
+
+  controller_nh.getParam("phi4_offset", phi4_offset_);
+  controller_nh.getParam("l1", l1_);
+  controller_nh.getParam("l2", l2_);
+  controller_nh.getParam("l3", l3_);
+  controller_nh.getParam("l4", l4_);
+  controller_nh.getParam("l5", l5_);
 
   active_suspension_sub_ = controller_nh.subscribe("/cmd_active_suspension", 10,
                                                    &ActiveSuspensionController::ActiveSuspensionCallBack, this);
-
-  XmlRpc::XmlRpcValue suspension_legs;
-  controller_nh.getParam("suspension_leg", suspension_legs);
-  stretch_coff_A_ = getParam(controller_nh, "stretch_coff_A_", 0.);
-  stretch_coff_k_ = getParam(controller_nh, "stretch_coff_k_", 0.);
-  shrink_coff_A_ = getParam(controller_nh, "shrink_coff_A_", 0.);
-  shrink_coff_k_ = getParam(controller_nh, "shrink_coff_k_", 0.);
-  feedforward_offset = getParam(controller_nh, "feed_forward_offset", 0.);
-  feedforward_effect_time_ = getParam(controller_nh, "feedforward_effect_time_", 0.);
-
-  for (const auto& suspension_leg : suspension_legs)
-  {
-    ros::NodeHandle nh_suspension = ros::NodeHandle(controller_nh, "suspension_leg/" + suspension_leg.first);
-    active_suspension_joints_.push_back(std::make_shared<effort_controllers::JointPositionController>());
-    if (!active_suspension_joints_.back()->init(effort_joint_interface_, nh_suspension))
-    {
-      ROS_ERROR("Failed to init active_suspension_joint");
-      return false;
-    }
-  }
   return true;
 }
+
 void ActiveSuspensionController::moveJoint(const ros::Time& time, const ros::Duration& period)
 {
   OmniController::moveJoint(time, period);
+
   switch (current_state_)
   {
     case State::DOWN:
-      target_pos_ = -0.05;
-      static_effort = -6.0;
+      target_leg_length_ = 0.18;
+      target_pitch = 0;
       break;
     case State::MID:
-      target_pos_ = 0.30;
-      static_effort = 9.0;
+      target_leg_length_ = 0.30;
+      target_pitch = -0.25;
       break;
     case State::UP:
-      target_pos_ = 0.80;
-      static_effort = 8.0;
+      target_leg_length_ = 0.40;
+      target_pitch = -0.3;
       break;
   }
-  if (current_state_ != last_state_)
-  {
-    last_state_ = current_state_;
-    feedforward_timer = ros::Time::now();
-  }
-  if ((ros::Time::now() - feedforward_timer).toSec() < feedforward_effect_time_)
-  {
-    if (current_state_ == State::MID)
-      feedforward_effort = 1.2 * (stretch_coff_A_ - 45.0 * current_pos_);
-    else if (current_state_ == State::UP)
-      feedforward_effort = 2 * (stretch_coff_A_ + stretch_coff_k_ * current_pos_);
-    else if (current_state_ == State::DOWN)
-      feedforward_effort = shrink_coff_A_ + shrink_coff_k_ * current_pos_;
-  }
-  else
-    feedforward_effort = 0.0;
 
-  for (auto& joint : active_suspension_joints_)
-  {
-    joint->setCommand(target_pos_);
-    joint->update(time, period);
-    current_pos_ = joint->getPosition();
-    auto cmd = joint->joint_.getCommand();
-    cmd += feedforward_effort + feedforward_offset + static_effort;
-    joint->joint_.setCommand(cmd);
-  }
+  // 0 is phi1, 1 is phi4
+  double left_angle[2]{ 0. }, right_angle[2]{ 0. };
+  left_angle[0] = right_angle[0] = 0;  // Todo:add real angle
+  left_angle[1] = left_hip_joint_handle_->getPosition() + phi4_offset_;
+  right_angle[1] = right_hip_joint_handle_->getPosition() + phi4_offset_;
+
+  calculateLegPos(left_angle[0], left_angle[1], left_pos_);
+  calculateLegPos(right_angle[0], right_angle[1], right_pos_);
+
+  double current_leg_length = (left_pos_[0] + right_pos_[0]) / 2.0;
+  double F_roll = pid_roll_->computeCommand(0 - roll_, period);
+  double F_pitch = pid_pitch_->computeCommand(target_pitch - pitch_, period);
+  double F_leg = pid_legs_->computeCommand(target_leg_length_ - current_leg_length, period);
+  double F_spring = calculateSpringForce();
+  double F_gravity = calculateFx_gravity();
+  double F_left{}, F_right{}, T_left{}, T_right{};
+
+  F_left = F_leg - F_roll - F_pitch - F_spring + F_gravity;
+  F_right = F_leg + F_roll - F_pitch - F_spring + F_gravity;
+
+  calculateTorque(F_left, left_angle[0], left_angle[1], T_left);
+  calculateTorque(F_right, right_angle[0], right_angle[1], T_right);
+
+  left_hip_joint_handle_->setCommand(T_left);
+  right_hip_joint_handle_->setCommand(T_right);
 }
-void ActiveSuspensionController::ActiveSuspensionCallBack(const rm_msgs::ChassisActiveSusCmd& msg)
+
+void ActiveSuspensionController::calculateLegPos(double phi1, double phi4, double pos[2])
 {
-  current_state_ = static_cast<State>(msg.mode);
+  // Four link vmc
+  double YD, YB, XD, XB, lBD, A0, B0, C0, XC, YC;
+  
+
 }
+
+void ActiveSuspensionController::calculateTorque(double F, double phi1, double phi4, double T)
+{
+  double J[1][2]{};
+  calculateJacobian(phi1, phi4, J);
+
+  // T=J^T * F
+  T = J[0][0] * F;
+}
+
+void ActiveSuspensionController::calculateJacobian(double phi1, double phi4, double J[1][2])
+{
+  // Four link vmc
+  double YD, YB, XD, XB, lBD, A0, B0, C0, XC, YC;
+  double phi2, phi3;
+  double L0, phi0;
+  double j01, j11;
+
+  YD = l4_ * sin(phi4);
+  YB = l1_ * sin(phi1);
+  XD = l4_ * cos(phi4);
+  XB = l1_ * cos(phi1);
+  lBD = sqrt((XD - XB) * (XD - XB) + (YD - YB) * (YD - YB));
+  A0 = 2 * l2_ * (XD - XB);
+  B0 = 2 * l2_ * (YD - YB);
+  C0 = l2_ * l2_ + lBD * lBD - l3_ * l3_;
+  phi2 = 2 * atan2((B0 + sqrt(A0 * A0 + B0 * B0 - C0 * C0)), A0 + C0);
+  phi3 = atan2(YB - YD + l2_ * sin(phi2), XB - XD + l2_ * cos(phi2));
+  XC = l1_ * cos(phi1) + l2_ * cos(phi2);
+  YC = l1_ * sin(phi1) + l2_ * sin(phi2);
+  L0 = sqrt(XC * XC + YC * YC);
+  phi0 = atan2(YC, XC);
+
+  j01 = (l4_ * sin(phi0 - phi2) * sin(phi3 - phi4)) / sin(phi3 - phi2);
+  j11 = (l4_ * cos(phi0 - phi2) * sin(phi3 - phi4)) / (L0 * sin(phi3 - phi2));
+
+  J[0][1] = j01;
+  J[1][1] = j11;
+}
+
+double ActiveSuspensionController::calculateSpringForce()
+{ return 0.; }
+
+double ActiveSuspensionController::calculateFx_gravity()
+{ return 0; }
+
+void ActiveSuspensionController::ActiveSuspensionCallBack(const rm_msgs::ChassisActiveSusCmd& msg)
+{ current_state_ = static_cast<State>(msg.mode); }
 }  // namespace rm_chassis_controllers
 PLUGINLIB_EXPORT_CLASS(rm_chassis_controllers::ActiveSuspensionController, controller_interface::ControllerBase)
